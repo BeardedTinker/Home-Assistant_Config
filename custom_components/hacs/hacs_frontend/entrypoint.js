7c1f1e174802f4c552330947880a05e334fc7005
root <root@core-ssh.local.hass.io>
2021-11-17 09:19:31 +0100

    HACS update to HACS #16 .

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-50818363.js')")();
+  new Function("import('/hacsfiles/frontend/main-8113c4f2.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-50818363.js';
+  el.src = '/hacsfiles/frontend/main-8113c4f2.js';
   document.body.appendChild(el);
 }
   
