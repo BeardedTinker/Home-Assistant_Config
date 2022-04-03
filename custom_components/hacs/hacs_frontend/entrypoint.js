0df1eab5933799cab8520157264846a2f805136c
Bearded Tinker <github@beardedtinker.com>
2022-04-03 13:28:24 +0200

    HAVS update to HACS #16

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-c486ac4b.js')")();
+  new Function("import('/hacsfiles/frontend/main-f3e781b1.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-c486ac4b.js';
+  el.src = '/hacsfiles/frontend/main-f3e781b1.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
