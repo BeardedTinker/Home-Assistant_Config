8c07482c1d28f6e3981892163228ff607e23f62c
Bearded Tinker <github@beardedtinker.com>
2023-03-10 22:54:30 +0100

    HACS update to Anniversaries #16

--- custom_components/anniversaries/const.py
+++ custom_components/anniversaries/const.py
@@ -9,7 +9,7 @@ from homeassistant.const import CONF_NAME
 # Base component constants
 DOMAIN = "anniversaries"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "5.0.0"
+VERSION = "5.1.0"
 PLATFORM = "sensor"
 ISSUE_URL = "https://github.com/pinkywafer/Anniversaries/issues"
 ATTRIBUTION = "Sensor data calculated by Anniversaries Integration"
