be9db0e3db7a671952c040989602ef68f8eed3d2
Bearded Tinker <github@beardedtinker.com>
2023-04-05 22:36:17 +0200

    HA update to 2023.4 #14

--- custom_components/spacex/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the SpaceX integration."""
-
-DOMAIN = "spacex"
-COORDINATOR = "coordinator"
-SPACEX_API = "spacex_api"
-ATTR_IDENTIFIERS = "identifiers"
-ATTR_MANUFACTURER = "manufacturer"
-ATTR_MODEL = "model"
