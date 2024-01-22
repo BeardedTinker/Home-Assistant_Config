3573d76933fc83890b1869c88a014280edb78d25
Bearded Tinker <github@beardedtinker.com>
2024-01-22 23:09:58 +0100

    HACS update to PowerCalc #16

--- custom_components/powercalc/sensor.py
+++ custom_components/powercalc/sensor.py
@@ -46,6 +46,7 @@ from .common import (
     validate_name_pattern,
 )
 from .const import (
+    CONF_ALL,
     CONF_AND,
     CONF_AREA,
     CONF_CALCULATION_ENABLED_CONDITION,
@@ -154,6 +155,7 @@ MAX_GROUP_NESTING_LEVEL = 5
 
 FILTER_CONFIG = vol.Schema(
     {
+        vol.Optional(CONF_ALL): None,
         vol.Optional(CONF_AREA): cv.string,
         vol.Optional(CONF_GROUP): cv.entity_id,
         vol.Optional(CONF_TEMPLATE): cv.template,
@@ -481,25 +483,25 @@ def register_entity_services() -> None:
 
     platform.async_register_entity_service(
         SERVICE_CALIBRATE_UTILITY_METER,
-        {vol.Required(CONF_VALUE): validate_is_number},  # type: ignore
+        {vol.Required(CONF_VALUE): validate_is_number},
         "async_calibrate",
     )
 
     platform.async_register_entity_service(
         SERVICE_CALIBRATE_ENERGY,
-        {vol.Required(CONF_VALUE): validate_is_number},  # type: ignore
+        {vol.Required(CONF_VALUE): validate_is_number},
         "async_calibrate",
     )
 
     platform.async_register_entity_service(
         SERVICE_INCREASE_DAILY_ENERGY,
-        {vol.Required(CONF_VALUE): validate_is_number},  # type: ignore
+        {vol.Required(CONF_VALUE): validate_is_number},
         "async_increase",
     )
 
     platform.async_register_entity_service(
         SERVICE_ACTIVATE_PLAYBOOK,
-        {vol.Required("playbook_id"): cv.string},  # type: ignore
+        {vol.Required("playbook_id"): cv.string},
         "async_activate_playbook",
     )
 
@@ -511,7 +513,7 @@ def register_entity_services() -> None:
 
     platform.async_register_entity_service(
         SERVICE_SWITCH_SUB_PROFILE,
-        {vol.Required("profile"): cv.string},  # type: ignore
+        {vol.Required("profile"): cv.string},
         "async_switch_sub_profile",
     )
 
